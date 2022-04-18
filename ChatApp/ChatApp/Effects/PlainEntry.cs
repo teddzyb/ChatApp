fe663f5f86015b9d30f6072740b39817c85b6466
teddzyb <teddybartletg88@gmail.com>
2022-04-18 12:50:06 +0800

    Added Plain Entry Effect

---
+++ ChatApp/ChatApp/Effects/PlainEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Calculator.Effects
+{
+    public class PlainEntry : RoutingEffect
+    {
+        public PlainEntry() : base("PlainEntryGroup.PlainEntryEffect")
+        {
+        }
+    }
+}
+
+/*
+    <Entry>
+        <Entry.Effects>
+            <effects:PlainEntry />
+        </Entry.Effects>
+    </Entry>
+ */
