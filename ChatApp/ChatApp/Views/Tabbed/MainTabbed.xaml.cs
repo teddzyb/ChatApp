6b73e1bc72669f73cfd0d76f2a998c96c73061fe
teddzyb <teddybartletg88@gmail.com>
2022-04-19 13:14:07 +0800

    Tabbed page navigation now functional

---
+++ ChatApp/ChatApp/Views/Tabbed/MainTabbed.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChatApp.Views.Tabbed
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbed : ContentView
+    {
+        public MainTabbed()
+        {
+            InitializeComponent();
+        }
+
+        private void Nav_Chat(object sender, EventArgs e)
+        {
+            this.FindByName<View>("Chat").IsVisible = true;
+            this.FindByName<View>("Profile").IsVisible = false;
+        }
+
+        private void Nav_Profile(object sender, EventArgs e)
+        {
+            this.FindByName<View>("Chat").IsVisible = false;
+            this.FindByName<View>("Profile").IsVisible = true;
+        }
+    }
+}
