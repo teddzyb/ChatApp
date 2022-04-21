c55bc95699c18edd8fa233ca84831b9d94d23058
teddzyb <teddybartletg88@gmail.com>
2022-04-21 12:24:25 +0800

    Initial chat page

--- ChatApp/ChatApp.Android/Resources/Resource.designer.cs
+++ ChatApp/ChatApp.Android/Resources/Resource.designer.cs
@@ -16688,22 +16688,25 @@ namespace ChatApp.Droid
 			public const int profile_enabled = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int show = 2131165334;
+			public const int search = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int splash_logo = 2131165335;
+			public const int show = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int splash_screen = 2131165336;
+			public const int splash_logo = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int test_custom_background = 2131165337;
+			public const int splash_screen = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int tooltip_frame_dark = 2131165338;
+			public const int test_custom_background = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_light = 2131165339;
+			public const int tooltip_frame_dark = 2131165339;
+			
+			// aapt resource value: 0x7F07009C
+			public const int tooltip_frame_light = 2131165340;
 			
 			static Drawable()
 			{
