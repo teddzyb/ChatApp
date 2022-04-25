81a8a5025bdb384896c1912d999338c17f8f07cf
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-04-26 00:01:57 +0800

    commit

--- ChatApp/ChatApp/Pages/Tabbed/Chats.xaml.cs
+++ ChatApp/ChatApp/Pages/Tabbed/Chats.xaml.cs
@@ -25,14 +25,14 @@ namespace ChatApp.Pages.Tabbed
             //chatList.Add(new ChatModel() { id = 2, senderEmail = "edwin@gmail.com", receiverEmail = "van@gmail,com", message = "HELLO wazzup" });
             //chatListView.ItemsSource = chatList;
 
-            userList.Add(new UserModel() { id = 1, username = "edwin", email = "ediwn@gmail.com", password = "123" });
-            userList.Add(new UserModel() { id = 2, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 3, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 4, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 5, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 6, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 7, username = "van", email = "van@gmail.com", password = "231" });
-            userList.Add(new UserModel() { id = 9, username = "van", email = "van@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 1, username = "User Test-1", email = "utest1@gmail.com", password = "123" });
+            userList.Add(new UserModel() { id = 2, username = "User Test-2", email = "utest2gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 3, username = "User Test-3", email = "utest3@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 4, username = "User Test-4", email = "utest4@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 5, username = "User Test-5", email = "utest5@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 6, username = "User Test-6", email = "utest6@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 7, username = "User Test-7", email = "utest7@gmail.com", password = "231" });
+            userList.Add(new UserModel() { id = 8, username = "User Test-8", email = "utest8@gmail.com", password = "231" });
             
 
 
