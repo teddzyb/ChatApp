ca50b93dcc9a37cea3755728a839e09205ac4315
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-04-28 00:03:44 +0800

    commit

--- ChatApp/ChatApp/Pages/Tabbed/SearchResults.xaml.cs
+++ ChatApp/ChatApp/Pages/Tabbed/SearchResults.xaml.cs
@@ -18,7 +18,8 @@ namespace ChatApp.Pages.Tabbed
         ObservableCollection<UserModel> userList = new ObservableCollection<UserModel>();
         ObservableCollection<UserResults> userResultsList = new ObservableCollection<UserResults>();
         public class UserResults
-        {
+        { 
+            public string id { get; set; }
             public string username { get; set; }
             public string email { get; set; }
             public string iconSource { get; set; }
@@ -52,36 +53,60 @@ namespace ChatApp.Pages.Tabbed
             if (string.IsNullOrEmpty(SearchEntry.Text))
             {
                 userListView.ItemsSource = null;
+                AlertLabel.IsVisible = false;
                 return;
             }
 
             string id = (string)Application.Current.Properties["id"];            
             var users = userList.Where(x => x.email.ToLower().Contains(SearchEntry.Text.ToLower()));
-            if (users.Count() > 0)
+            if (users.Count() == 0)
             {
-                var userFriends = userList.Where(x => x.id == id).First().contacts;
+                AlertLabel.IsVisible = true;
+                return;
+            } 
+            
+            AlertLabel.IsVisible = false;
+            var userFriends = userList.Where(x => x.id == id).First().contacts;
 
-                foreach (var user in users)
+            foreach (var user in users)
+            {
+                string iconSource = "add";
+
+                if (userFriends.Where(x => x == user.id).Count() > 0)
                 {
-                    string iconSource = "add";
-
-                    if (userFriends.Where(x => x == user.id).Count() > 0)
-                    {
-                        iconSource = "check";                           
-                    }
-
-                    if (user.id == id)
-                    {
-                        iconSource = "close_round";
-                    }
-
-                    userResultsList.Add(new UserResults { username = user.username, email = user.email, iconSource = iconSource });
+                    iconSource = "check";
                 }
 
-                userListView.ItemsSource = userResultsList;
-            }                
-                
-            
+                if (user.id == id)
+                {
+                    iconSource = "close_round";
+                }
+
+                userResultsList.Add(new UserResults { id = user.id, username = user.username, email = user.email, iconSource = iconSource });
+            }
+
+            userListView.ItemsSource = userResultsList;
+        }
+    
+        private async void  AddToContact(object sender, EventArgs e)
+        {
+            string searchID = (string)((TappedEventArgs)e).Parameter;
+            string userID = (string)Application.Current.Properties["id"];
+
+            if (searchID.Equals(userID)) {
+                await DisplayAlert("Error", "You are not allowed to add yourself.", "","OKAY");
+                return;
+            }
+
+            var userFriends = userList.Where(x => x.id == userID).First().contacts;
+            if (userFriends.Where(x => x == searchID).Count() > 0)
+            {
+                await DisplayAlert("Failed", "You already have a connection.", "", "OKAY");
+                return;
+            }
+
+
         }
     }
+
 }
