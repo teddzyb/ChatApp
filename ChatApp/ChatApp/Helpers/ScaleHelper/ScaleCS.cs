15ef4aba653bd41de9e60c558e5717b342e7d222
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-05-07 01:15:44 +0800

    scale helper

---
+++ ChatApp/ChatApp/Helpers/ScaleHelper/ScaleCS.cs
@@ -0,0 +1,121 @@
+﻿using ChatApp;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public static class ScaleCS
+    {
+        /// <summary>
+        /// Scales the height.
+        /// </summary>
+        /// <returns>The height.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleHeight(this int number, int? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenHeight / 568.0));
+        }
+
+        /// <summary>
+        /// Scales the height.
+        /// </summary>
+        /// <returns>The height.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleHeight(this double number, double? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenHeight / 568.0));
+        }
+
+        /// <summary>
+        /// Scales the height.
+        /// </summary>
+        /// <returns>The height.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleHeight(this float number, float? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenHeight / 568.0));
+        }
+
+        /// <summary>
+        /// Scales the width.
+        /// </summary>
+        /// <returns>The width.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleWidth(this int number, int? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenWidth / 320.0));
+        }
+
+        /// <summary>
+        /// Scales the width.
+        /// </summary>
+        /// <returns>The width.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleWidth(this double number, double? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenWidth / 320.0));
+        }
+
+        /// <summary>
+        /// Scales the width.
+        /// </summary>
+        /// <returns>The width.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static float ScaleWidth(this float number, float? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (float)(number * (App.screenWidth / 320.0));
+        }
+
+        /// <summary>
+        /// Scales the font.
+        /// </summary>
+        /// <returns>The font.</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static double ScaleFont(this int number, int? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (number * (App.screenHeight / 568.0) - (Device.RuntimePlatform == Device.iOS ? 0.5 : 0));
+        }
+
+        /// <summary>
+        /// Scales the font.
+        /// </summary>
+        /// <returns>Formatted chips</returns>
+        /// <param name="number">Number.</param>
+        /// <param name="iOS">Value for iOS.</param>
+        public static double ScaleFont(this double number, double? iOS = null)
+        {
+            if (iOS.HasValue && Device.RuntimePlatform == Device.iOS)
+                number = iOS.Value;
+
+            return (number * (App.screenHeight / 568.0) - (Device.RuntimePlatform == Device.iOS ? 0.5 : 0));
+        }
+    }
+}
---
+++ ChatApp/ChatApp/Helpers/ScaleHelper/ScaleXAML.cs
@@ -0,0 +1,94 @@
+﻿using ChatApp;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public class ScaleXAML : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter.ToString().Contains("="))
+            {
+                var paramType = parameter.ToString().Remove(parameter.ToString().IndexOf("=", StringComparison.CurrentCulture));
+                var param = parameter.ToString().Remove(0, paramType.Length + 1);
+
+                if (param.Contains("|"))
+                {
+                    string[] paramPlatform = param.Split('|');
+                    param = Device.RuntimePlatform == Device.Android ? paramPlatform[0] : paramPlatform[1];
+                }
+
+                double convertedValue;
+
+                if (paramType.Contains("height"))
+                {
+                    if (double.TryParse(param, NumberStyles.Number, CultureInfo.InvariantCulture, out convertedValue))
+                        return convertedValue.ScaleHeight();
+                }
+                else if (paramType.Contains("width"))
+                {
+                    if (double.TryParse(param, NumberStyles.Number, CultureInfo.InvariantCulture, out convertedValue))
+                        return convertedValue.ScaleWidth();
+                }
+                else if (paramType.Contains("thickness"))
+                {
+                    return ConvertToThicknessProperty(param);
+                }
+                else if (paramType.Contains("fontSize"))
+                {
+                    if (double.TryParse(param, NumberStyles.Number, CultureInfo.InvariantCulture, out convertedValue))
+                        return convertedValue.ScaleHeight() - (Device.RuntimePlatform == Device.iOS ? 0.5 : 0);
+                }
+
+                throw new InvalidOperationException($"[{parameter.ToString()}] is an Invalid parameters. Supported parameters are height, width, thickness, fontSize, absolute-WH, absolute-NONE");
+            }
+
+            // default
+            if (parameter.ToString().Contains(",") == true)
+            {
+                return ConvertToThicknessProperty(parameter.ToString());
+            }
+
+            return (double.Parse(parameter.ToString()) * (App.screenHeight / 568.0));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        object ConvertToThicknessProperty(string param)
+        {
+            double l, t, r, b;
+            string[] thickness = param.Split(',');
+
+            if (thickness.Length == 4)
+            {
+                if (double.TryParse(thickness[0], NumberStyles.Number, CultureInfo.InvariantCulture, out l) && double.TryParse(thickness[1], NumberStyles.Number, CultureInfo.InvariantCulture, out t) && double.TryParse(thickness[2], NumberStyles.Number, CultureInfo.InvariantCulture, out r) && double.TryParse(thickness[3], NumberStyles.Number, CultureInfo.InvariantCulture, out b))
+                {
+                    return new Thickness(l * (App.screenWidth / 320.0), t * (App.screenHeight / 568.0), r * (App.screenWidth / 320.0), b * (App.screenHeight / 568.0));
+                }
+            }
+            throw new InvalidOperationException("Cannot convert thickness");
+        }
+
+        object ConvertToPlatformThicknessProperty(string param)
+        {
+            string[] paramPlatform = param.Split('|');
+            double l, t, r, b;
+            string[] thickness = Device.RuntimePlatform == Device.Android ? paramPlatform[0].Split(',') : paramPlatform[1].Split(',');
+
+
+            if (thickness.Length == 4)
+            {
+                if (double.TryParse(thickness[0], NumberStyles.Number, CultureInfo.InvariantCulture, out l) && double.TryParse(thickness[1], NumberStyles.Number, CultureInfo.InvariantCulture, out t) && double.TryParse(thickness[2], NumberStyles.Number, CultureInfo.InvariantCulture, out r) && double.TryParse(thickness[3], NumberStyles.Number, CultureInfo.InvariantCulture, out b))
+                {
+                    return new Thickness(l * (App.screenWidth / 320.0), t * (App.screenHeight / 568.0), r * (App.screenWidth / 320.0), b * (App.screenHeight / 568.0));
+                }
+            }
+            throw new InvalidOperationException("Cannot convert platform-thickness");
+        }
+    }
+}
